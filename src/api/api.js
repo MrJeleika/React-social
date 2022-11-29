a49f6027ccd8b4dca2d2f2127510738ab00088e0
MrJeleika <den932005@gmail.com>
2022-11-29 13:08:05 +0200

    Deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-import * as axios from 'axios';
+import axios from 'axios'
 
 const instance = axios.create({
   withCredentials: true,
@@ -6,71 +6,71 @@ const instance = axios.create({
   headers: {
     'API-KEY': 'a5d9edb8-4508-410c-a678-b4c99c2b4972',
   },
-});
+})
 
 export const APIgetUsers = (currentPage = 1) => {
   return instance.get(`users?page=${currentPage}`).then((response) => {
-    return response.data;
-  });
-};
+    return response.data
+  })
+}
 export const APIgetUserProfile = (userId) => {
   return instance.get(`profile/${userId}`).then((response) => {
-    return response.data;
-  });
-};
+    return response.data
+  })
+}
 export const APIgetMyProfile = () => {
   return instance.get(`auth/me`).then((response) => {
     return instance
       .get(`profile/${response.data.data.id}`)
       .catch((err) => console.log(err))
       .then((response) => {
-        return response.data;
-      });
-  });
-};
+        return response.data
+      })
+  })
+}
 // Check is user autorithed
 export const APIcheckIsAuth = () => {
   return instance.get('auth/me').then((response) => {
-    return response.data.resultCode === 0 ? true : false;
-  });
-};
+    return response.data.resultCode === 0 ? true : false
+  })
+}
 export const APIupdateProfileStatus = (status = '') => {
   return instance
     .put('profile/status', { status: status })
-    .then((response) => {});
-};
+    .then((response) => {})
+}
 export const APIgetProfileStatus = () => {
   return instance.get(`auth/me`).then((response) => {
     return instance
       .get(`profile/status/${response.data.data.id}`)
       .then((response) => {
-        return response.data;
-      });
-  });
-};
+        return response.data
+      })
+  })
+}
 export const APIlogin = (formData) => {
   return instance.post('auth/login', formData).then((response) => {
-    console.log(response);
-    return response.data.resultCode;
-  });
-};
+    console.log(response)
+    return response.data.resultCode
+  })
+}
 export const APIlogout = () => {
   return instance.delete('auth/login').then((response) => {
-    console.log(response);
-  });
-};
+    console.log(response)
+  })
+}
 export const APIfollow = (id) => {
   return instance.post(`follow/${id}`).then((response) => {
-    return response.data.resultCode;
-  });
-};
+    return response.data.resultCode
+  })
+}
 export const APIunfollow = (id) => {
   return instance.delete(`follow/${id}`).then((response) => {
-    return response.data.resultCode;
-  });
-};
+    return response.data.resultCode
+  })
+}
 export const APIisFollow = (id) => {
   return instance.get(`follow/${id}`).then((response) => {
-    return response.data;
-  });
-};
+    return response.data
+  })
+}
